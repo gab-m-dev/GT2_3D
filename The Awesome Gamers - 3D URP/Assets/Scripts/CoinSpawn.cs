c6bb9460d9aeb7a6ec25164df6a3b656cedb6628
CoalaCode <max.schneider@student.htw-berlin.de>
2023-02-03 14:31:37 +0100

    Player movement roll/pitch/yaw update and coin spawn fixed

--- The Awesome Gamers - 3D URP/Assets/Scripts/CoinSpawn.cs
+++ The Awesome Gamers - 3D URP/Assets/Scripts/CoinSpawn.cs
@@ -10,54 +10,56 @@ public class CoinSpawn : MonoBehaviour
     //reusable pool of coins => reduces the number of objects that are created
     private Queue<GameObject> coinPool = new Queue<GameObject>();
 
-     void Start()
+    void Start()
     {
-       StartCoroutine(spawnCoins(10));
+        StartCoroutine(spawnCoins(10));
     }
 
-  
-   IEnumerator  spawnCoins(int numberOfCoins){
 
-        
-        for(int i = 0; i < numberOfCoins; i++){
+    IEnumerator spawnCoins(int numberOfCoins)
+    {
+
+
+        for (int i = 0; i < numberOfCoins; i++)
+        {
             // pause after each iteration => allows for coins to be spawned over multiple frames
             yield return new WaitForEndOfFrame();
-            if(coinPool.Count > 0){
+            if (coinPool.Count > 0)
+            {
                 temp = coinPool.Dequeue();
             }
-                
+
             temp = Instantiate(coinPrefab);
             temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>()); // set the pos of the just spawned coin equal to a random position in the collider
         }
     }
 
-   Vector3 GetRandomPointInCollider(Collider collider) {
-    Vector3 min = collider.bounds.min;
-    Vector3 max = collider.bounds.max;
+    Vector3 GetRandomPointInCollider(Collider collider)
+    {
+        Vector3 min = collider.bounds.min;
+        Vector3 max = collider.bounds.max;
 
-    // Generate a random position within the bounds of the collider using min and max
-    Vector3 pos = new Vector3(
-        Random.Range(min.x, max.x),
-        Random.Range(min.y, max.y),
-        Random.Range(min.z, max.z)
-    );
+        // Generate a random position within the bounds of the collider using min and max
+        Vector3 pos = new Vector3(
+            Random.Range(min.x + 5, max.x - 5),
+            Random.Range(min.y + 5, max.y - 5),
+            Random.Range(min.z, max.z)
+        );
 
-    // Ensure that the position is on the collider
-    pos = collider.ClosestPoint(pos);
+        // Ensure that the position is on the collider
+        pos = collider.ClosestPoint(pos);
 
-    // Set the y value of the position to 1, otherwise coins spawn inside the ground
-    pos.y = Random.Range(min.y, max.y);
+        // Set the y value of the position to 1, otherwise coins spawn inside the ground
+        pos.y = Random.Range(min.y + 5, max.y + 5);
 
-    return pos;
-}
+        return pos;
+    }
 
-// when a coin is no longer needed, it can be returned to the pool to be reused later
-    public void ReturnCoinToPool(GameObject coin){
-    
+    // when a coin is no longer needed, it can be returned to the pool to be reused later
+    public void ReturnCoinToPool(GameObject coin)
+    {
+
         coin.SetActive(false);
         coinPool.Enqueue(coin);
     }
-  
-
-    
 }
--- The Awesome Gamers - 3D URP/Assets/Scripts/DemoPlayerMovement.cs
+++ The Awesome Gamers - 3D URP/Assets/Scripts/DemoPlayerMovement.cs
@@ -9,18 +9,26 @@ public class DemoPlayerMovement : MonoBehaviour
     [SerializeField] float forwardSpeed;
     private Rigidbody rigidbody;
     private Vector3 movement;
+    private float rotation;
+    private float rotationSpeed = 1f;
+    private Quaternion originalRotation;
+    private Quaternion qTo;
+
 
     //DISTANCE
     private float distance;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
-         Time.timeScale = 1f;
-       
+        Time.timeScale = 1f;
+
+        originalRotation = transform.GetChild(1).rotation;
+        qTo = transform.GetChild(1).rotation;
+
         rigidbody = transform.GetComponent<Rigidbody>();
+
         //DISTANCE
         StartCoroutine(distanceTracker());
     }
@@ -30,28 +38,39 @@ public class DemoPlayerMovement : MonoBehaviour
     {
         // Forward Movement
         transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed, Space.World);
+
+        // Vertical/horizontal movement
         movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
-        /*
-        //Tilt spaceship when moving left and right
-        float z = Input.GetAxis("Horizontal") * 15.0f; // might be negative, just test it
-        Vector3 euler = transform.localEulerAngles;
-        euler.z = Mathf.Lerp(euler.z, z, 2.0f * Time.deltaTime);
-        transform.localEulerAngles = euler;
-        */
+        // Horizontal movement rotation
+        rotation += Input.GetAxis("Horizontal");
+        rotation = Mathf.Clamp(rotation, -10f, 10f);
+        transform.GetChild(1).localEulerAngles = new Vector3(0, 0, -rotation);
 
-        
+        var zQuaternion = Quaternion.AngleAxis(rotation, Vector3.forward);
+        qTo = originalRotation * zQuaternion;
+
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            //transform.GetChild(1).rotation = Quaternion.identity;
+            //Debug.Log("Halloooooooo");
+            var step = rotationSpeed * Time.deltaTime;
+            transform.GetChild(1).rotation = Quaternion.Slerp(transform.GetChild(1).rotation, qTo, step);
+        }
     }
 
 
     // Änderungen an Rigidbody werden in Fixed Update gemacht
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         rigidbody.velocity = movement * horizontalSpeed;
     }
 
     //DISTANCE
-    private IEnumerator distanceTracker() {
-        while(true){
+    private IEnumerator distanceTracker()
+    {
+        while (true)
+        {
             //S = V * t
             //distance = Mathf.Round(forwardSpeed * Time.deltaTime);
             Highscore.inst.increaseScore(1.0f);
